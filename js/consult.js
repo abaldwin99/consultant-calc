dd5c1346fdc444841b6683969a72379b3ca31107
Alex Baldwin <abaldwintech@gmail.com>
2015-07-03 22:16:03 -0400

    Product Sub Total calculating

--- js/consult.js
+++ js/consult.js
@@ -9,10 +9,28 @@ angular.module('consultCalc', [])
 
         consult.addItemRow = function () {
             consult.itemRows.push({
-                amount: '',
-                quantity: ''
+                amount: null,
+                quantity: null
             })
         };
+
+        consult.calcProductSubTotal = function () {
+            var productSubTotal = 0;
+            var rowTotal;
+            for (var i = 0; i < consult.itemRows.length; i++) {
+                rowTotal = consult.itemRows[i].amount * consult.itemRows[i].quantity;
+                productSubTotal += rowTotal ? rowTotal : 0;
+            };
+            var discount = (consult.discount ? consult.discount / 100 - 1 : 1);
+            productSubTotal = productSubTotal * discount;
+            consult.productSubTotal = productSubTotal;
+        };
+
+        consult.calcFields = function () {
+            consult.calcProductSubTotal()
+        };
+
+
 
         var init = function () {
             for (var i = 0; i < 3; i++) {
